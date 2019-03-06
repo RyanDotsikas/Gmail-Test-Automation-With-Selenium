b5089ab9d83a27002827497f7f1df716a6c2b7ad
Matthew Caminiti <matthew.caminiti@mail.mcgill.ca>
2019-03-06 16:03:03 -0500

    Added multi-attach send

--- email_testing.py
+++ email_testing.py
@@ -60,6 +60,40 @@ def send_mail(recipient_address, email_subject, path_to_attachment):
 	driver.quit()
 	return email
 
+def send_mail_multi_attach(recipient_address, email_subject, paths_to_attachments):
+	driver = load_inbox()
+
+	create_mail_button = driver.find_element_by_css_selector(".T-I.J-J5-Ji.T-I-KE.L3")
+	create_mail_button.click()
+
+	to_field = driver.find_element_by_css_selector(".vO")
+	to_field.send_keys(recipient_address)
+
+	subject_field = driver.find_element_by_css_selector(".aoT")
+	subject_field.send_keys(email_subject)
+
+	emailbody_field = driver.find_element_by_css_selector(".Am.Al.editable.LW-avf")
+	emailbody_field.send_keys("Yarr harr I am an email spam bot ayy lmao " + str(random.randint(1,1000)))
+	time.sleep(3) # NEED TO FIGURE OUT PROPER WAIT FOR LOAD
+
+	for path in paths_to_attachments:
+		file_entry = driver.find_element_by_name("Filedata")
+		file_entry.send_keys(path)
+		time.sleep(1) # NEED TO FIGURE OUT PROPER WAIT FOR LOAD
+
+	send_button = driver.find_element_by_css_selector(".T-I.J-J5-Ji.aoO.T-I-atl.L3")
+	send_button.click()
+	time.sleep(2) # NEED TO FIGURE OUT PROPER WAIT FOR LOAD
+
+	email = {}
+	email["delivery_address"] = "dogwizard69@gmail.com"
+	email["recipient_address"] = recipient_address
+	email["email_subject"] = email_subject
+	email["attachment_name"] = path_to_attachment.split("/")[len(path_to_attachment.split("/")) - 1]
+
+	driver.quit()
+	return email
+
 # email : dictionary containing delivery_address, recipient_address, email_subject, attachment_name
 # returns boolean of whether or not email is in the inbox
 def receive_mail(receiver_address, email):
